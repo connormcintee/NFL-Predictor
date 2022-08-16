9496e4bf84bb21d9a396da979f24c10302fbc0df
connormcintee <85814997+connormcintee@users.noreply.github.com>
2022-08-16 13:48:30 -0500

    Delete Combine_analysis.R

--- Combine_analysis.R
+++
@@ -1,9 +0,0 @@
-library(readxl)
-library(rpart)
-library(dplyr)
-data1=read_xlsx('~/Downloads/football_data/Combine_data.xlsx')
-data1
-data=as.data.frame(data1)
-data
-data2=filter(data, Pos == "RB")
-plot(data2$BenchReps)
